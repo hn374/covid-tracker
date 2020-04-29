4e91af660fd29e04f8ba3f7c4eb7b6b7dbc43dcb
Hoang Nguyen <mizore742@hotmail.com>
2020-04-29 16:22:06 -0400

    Added structure for globalData component

--- src/Screens/Global.js
+++ src/Screens/Global.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import './Global.css';
 import GlobalData from '../components/globalData.js';
 
 class Global extends Component {
     render() {
         return(
-            <div className="header">
-                <h2>Global</h2>
-                <GlobalData />
+            <div>
+                <h2 className="header">Global</h2>
+                <GlobalData title="New Confirmed Cases:"/>
+                <GlobalData title="Total Confirmed Cases:"/>
+                <GlobalData title="New Deaths:"/>
+                <GlobalData title="Total Deaths:"/>
+                <GlobalData title="New Recovered:"/>
+                <GlobalData title="Total Recovered:"/>
             </div>
         );
     }
--- src/components/globalData.js
+++ src/components/globalData.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import './globalData.css';
 
 class GlobalData extends Component {
+    constructor(props) {
+        super(props);
+        // const [data, setData] = useState(""); might have to put outside of constructor
+    }
+
+    // getDisplayData() {
+    //     // Get data from API using Axios
+    // }
+
     render() {
         return(
             <div>
-                <h2>Global Data</h2>
+                <h2 className="header">{ this.props.title }</h2>
+                <div className="displayData">Data</div>
+                {/* <div>{ data }</div> */}
             </div>
         );
     }
